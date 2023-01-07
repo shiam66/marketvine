30b6b0015e2b6cbcb2b17b61060ca32d1c7b9dcc
AIS-Infomatix <94098685+AIS-Infomatix@users.noreply.github.com>
2023-01-07 10:35:42 +0600

    seles budget

--- resources/views/frontEnd/saleBudget/saleBudget.blade.php
+++ resources/views/frontEnd/saleBudget/saleBudget.blade.php
@@ -302,6 +302,7 @@
                     method: "POST",
                     data: {yearId: yearId, _token: _token},
                     success: function (result) {
+                        console.log(result);
                         $('#tableData').html(result)
                     }
                 })
